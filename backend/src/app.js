23dfd28c7710c58daf17cd15061c16ce7c2021e5
erlaaaand <erlandagsya2@gmail.com>
2025-12-07 00:17:13 +0700

    update swagger on app.js

---
+++ backend/src/app.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import cors from 'cors';
+import taskRoutes from './routes/task.routes.js';
+import dotenv from 'dotenv';
+
+import swaggerUi from 'swagger-ui-express';
+import swaggerSpec from './config/swagger.js';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.get('/api-docs.json', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpec);
+});
+
+app.use('/api/tasks', taskRoutes);
+
+app.get('/', (req, res) => {
+    res.send('✅ Task Management API is Ready!');
+});
+
+export default app;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,9 @@
-import express from "express";
+import app from './app.js'
 import dotenv from "dotenv";
 dotenv.config();
 
-const app = express();
-
-app.use(express.json());
-
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, ()=>{
     console.log(`SERVER RUNNING : ${PORT}`);
 })
