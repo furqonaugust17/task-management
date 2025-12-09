6ce4502f0b80a97650002abf8d2adc43df66d772
erlaaaand <erlandagsya2@gmail.com>
2025-12-09 18:52:40 +0700

    add calendar routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import taskRoutes from './routes/task.routes.js';
+import calendarRoutes from './routes/calendar.routes.js';
 import dotenv from 'dotenv';
 
 import swaggerUi from 'swagger-ui-express';
@@ -13,6 +14,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use('/api/tasks', taskRoutes);
+app.use('/api/calendar', calendarRoutes); // Tambahkan baris ini
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.get('/api-docs.json', (req, res) => {
