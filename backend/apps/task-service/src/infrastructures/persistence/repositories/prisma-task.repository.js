d4cf5c1762e57454271f96dae3eca6ae9fec7d3d
erlaaaand <erlandagsya2@gmail.com>
2025-12-11 16:18:12 +0700

    backup

---
+++ backend/apps/task-service/src/infrastructures/persistence/repositories/prisma-task.repository.js
@@ -0,0 +1,47 @@
+import prisma from '../../config/database.js';
+
+export const findAllTasks = async () => {
+    return await prisma.taskManagement.findMany({
+        where: {
+            deletedAt: null
+        },
+        orderBy: { createdAt: 'desc' }
+    });
+};
+
+export const findTaskById = async (id) => {
+    return await prisma.taskManagement.findFirst({
+        where: {
+            id: id,
+            deletedAt: null
+        }
+    });
+};
+export const createTask = async (data) => {
+    return await prisma.taskManagement.create({
+        data
+    });
+};
+
+export const updateTask = async (id, data) => {
+    return await prisma.taskManagement.update({
+        where: { id },
+        data
+    });
+};
+
+export const deleteTask = async (id) => {
+    return await prisma.taskManagement.update({
+        where: { id },
+        data: {
+            deletedAt: new Date()
+        }
+    });
+};
+
+export const restoreTask = async (id) => {
+    return await prisma.taskManagement.update({
+        where: { id },
+        data: { deletedAt: null }
+    });
+};
